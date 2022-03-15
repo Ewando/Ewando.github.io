dee9d410b3ea6dfcf3eba3911360a4e20e85cd69
Ewando <55455275+Ewando@users.noreply.github.com>
2022-03-15 17:39:43 +0000

    test

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
+
 $(document).ready(function(){
+   
+    $('#bot p').delay(3000).fadeOut(500, function() {
+        $(this).text("Bloom helps people to track and achieve small to medium goals everyday and medium to large goals every week.").fadeIn(500);
+        $("#bot img").attr("src","img/progress2.png");
 
-    $("#introContainer").on( "swipe", swipeHandler );
+
+        $('#bot p').delay(4000).fadeOut(500, function() {
+            $(this).text("By doing so we aim to promote health and productivity for our users. Interested in joining?").fadeIn(500);
+            $("#bot img").attr("src","img/progress3.png");
+
+            $('#accountReg').delay(2000).animate({bottom: '15%'});
+              
+          });
+      
 
 
-    function swipeHandler( event ){
-        alert("I am an alert box!");
-      }
+      });
 
-});
+     
 
-$(document).on('swipeleft swiperight', function () {
-    alert("I am an alert box!");
-});
+  });
