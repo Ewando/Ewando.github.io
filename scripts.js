5150e1d19d7c9f531a2d1726aa30bd07ad79497b
Ewando <55455275+Ewando@users.noreply.github.com>
2022-03-16 01:54:22 +0000

    v3

--- scripts.js
+++ scripts.js
@@ -2,16 +2,18 @@
 $(document).ready(function(){
    
     $('#bot p').delay(3000).fadeOut(500, function() {
+
         $(this).text("Bloom helps people to track and achieve small to medium goals everyday and medium to large goals every week.").fadeIn(500);
         $("#bot img").attr("src","img/progress2.png");
+        $("#yoga").attr("src","img/yoga2.png");
 
 
         $('#bot p').delay(4000).fadeOut(500, function() {
-            $(this).text("By doing so we aim to promote health and productivity for our users. Interested in joining?").fadeIn(500);
-            $("#bot img").attr("src","img/progress3.png");
 
+            $(this).text("By doing so we aim to promote health and productivity for our users. Interested in blooming?").fadeIn(500);
+            $("#bot img").attr("src","img/progress3.png");
             $('#accountReg').delay(2000).animate({bottom: '25%'});
-              
+            $('#logo').fadeIn();
           });
       
 
